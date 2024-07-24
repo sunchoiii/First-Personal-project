bf5b10adef744512eaecbba3f37ba5ad69c288d9
Robbie <robbie331@>
2024-07-24 17:15:19 +0900

    Level 1 - 3 입력받은 수와 사칙연산기호를 사용해 연산 실행

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -17,5 +17,30 @@ public class App {
         // charAt(index) : charAt 메서드는 매개변수로 char 타입으로 반환하고자 하는 문자열의 위치(index)를 받는다
         char operator = sc.next().charAt(0);
 
+        //입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다.
+        int result = 0;
+
+        switch (operator) {
+            case '+' :  result = num1 + num2;
+                        break;
+
+            case '-' :  result = num1 - num2;
+                        break;
+
+            case '*' :  result = num1 * num2;
+                        break;
+
+            case '/' :  if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        break;
+                        }
+                        result = num1 / num2;
+                        break;
+
+            default :
+                System.out.println("잘못된 기호를 입력하셨습니다.");
+        }
+        System.out.println("결과: " + result);
+
     }
 }
