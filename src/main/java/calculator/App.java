0d990ffff9e4958e2eae3137135a63ff355b2f8e
Robbie <robbie331@>
2024-07-24 16:42:24 +0900

    Level 1 - 1 스캐너 양의 정수 2개 입력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,17 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("안녕하세요!!!");
+        Scanner sc = new Scanner(System.in);
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
     }
 }
